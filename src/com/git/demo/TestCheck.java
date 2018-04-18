321fe48979851e77a840e46e47a3917b0de625d9
13501140750 <421533518@qq.com>
2018-04-18 11:42:27 +0800

    test

---
+++ src/com/git/demo/TestCheck.java
@@ -0,0 +1,12 @@
+package com.git.demo; 
+/** 
+* @author 作者 wangyawei: 
+* @version 创建时间：2018年4月18日 上午11:41:56 
+* 类说明 
+*/
+public class TestCheck {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
+ 
